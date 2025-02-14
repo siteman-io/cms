4bad3182627e3ed79d0e55694684062b0d66eb16
bambamboole <manuel.christlieb@xentral.com>
2025-02-14 18:13:35 +0100

    Refactor wrapper and move menus to the layout.

--- resources/views/themes/blank/layouts/base.blade.php
+++ resources/views/themes/blank/layouts/base.blade.php
@@ -4,6 +4,22 @@
     */
     $renderer = app(\Siteman\Cms\Blocks\BlockRenderer::class)
 @endphp
+<header>
+    <ul>
+        @foreach(\Siteman\Cms\Facades\Siteman::getMenuItems('header') as $item)
+            <li><a href="{{$item->url}}" class="hover:underline">>{{$item->title}}</a></li>
+        @endforeach
+    </ul>
+</header>
+<main>
 @foreach($post->blocks ?? [] as $block)
     {!! $renderer->render($block, $post)  !!}
 @endforeach
+</main>
+<footer>
+    <ul>
+        @foreach(\Siteman\Cms\Facades\Siteman::getMenuItems('footer') as $item)
+            <li><a href="{{$item->url}}" class="hover:underline">>{{$item->title}}</a></li>
+        @endforeach
+    </ul>
+</footer>
--- resources/views/themes/blank/wrapper.blade.php
+++ resources/views/themes/blank/wrapper.blade.php
@@ -6,29 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     @yield('seo')
-    <style>
-        body{
-            margin: 0;
-        }
-    </style>
 </head>
 <body>
-<header>
-    <ul>
-        @foreach(\Siteman\Cms\Facades\Siteman::getMenuItems('header') as $item)
-            <li><a href="{{$item->url}}" class="hover:underline">>{{$item->title}}</a></li>
-        @endforeach
-    </ul>
-</header>
-<main>
-    @yield('content')
-</main>
-<footer>
-    <ul>
-        @foreach(\Siteman\Cms\Facades\Siteman::getMenuItems('footer') as $item)
-            <li><a href="{{$item->url}}" class="hover:underline">>{{$item->title}}</a></li>
-        @endforeach
-    </ul>
-</footer>
+
+@yield('content')
+
 </body>
 </html>
