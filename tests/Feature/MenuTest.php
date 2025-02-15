c83442196d3a045ea38d8afa47e6acf284eebd83
bambamboole <manuel.christlieb@xentral.com>
2025-02-15 13:03:38 +0100

    Add more menu tests

--- tests/Feature/MenuTest.php
+++ tests/Feature/MenuTest.php
@@ -59,8 +59,24 @@ it('can update menus', function () {
     expect($menu->refresh())->name->toBe('New Name');
 });
 
-it('needs permission to delete menus', function () {})->todo();
-it('can delete menus', function () {})->todo();
+it('needs permission to delete menus', function () {
+    actingAs(User::factory()->withPermissions(['view_any_menu', 'update_menu'])->create());
+    $menu = Menu::factory()->create(['name' => 'Old Name']);
+
+    livewire(EditMenu::class, ['record' => $menu->id])
+        ->assertActionHidden('delete');
+});
+
+it('can delete menus', function () {
+    actingAs(User::factory()->withPermissions(['view_any_menu', 'update_menu', 'delete_menu'])->create());
+    $menu = Menu::factory()->create(['name' => 'Old Name']);
+
+    livewire(EditMenu::class, ['record' => $menu->id])
+        ->callAction('delete')
+        ->assertHasNoActionErrors();
+
+    expect(Menu::count())->toBe(0);
+});
 
 it('can create menu items', function () {})->todo();
 
