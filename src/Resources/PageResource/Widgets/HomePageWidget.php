f5d7d3cdcfc48795eb12adf49b1d2b6bbb886172
bambamboole <manuel.christlieb@xentral.com>
2025-02-23 21:41:23 +0100

    refactor page view paths

--- src/Resources/PageResource/Widgets/HomePageWidget.php
+++ src/Resources/PageResource/Widgets/HomePageWidget.php
@@ -6,5 +6,5 @@ use Filament\Widgets\Widget;
 
 class HomePageWidget extends Widget
 {
-    protected static string $view = 'siteman::resources.page-resource.widgets.home-page-widget';
+    protected static string $view = 'siteman::resources.page.widgets.home-page-widget';
 }
