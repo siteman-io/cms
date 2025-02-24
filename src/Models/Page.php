bb4089570eb2c26076dc619b1e0485b22a0b6e89
bambamboole <manuel.christlieb@xentral.com>
2025-02-24 18:54:48 +0100

    Add parent and children relation to model

--- src/Models/Page.php
+++ src/Models/Page.php
@@ -2,9 +2,21 @@
 
 namespace Siteman\Cms\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Siteman\Cms\Database\Factories\PageFactory;
 
 class Page extends BasePostType
 {
     protected static string $factory = PageFactory::class;
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(static::class);
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(static::class, 'parent_id');
+    }
 }
