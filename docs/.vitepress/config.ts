9e79eb1a1a2f95de0b1db3ba833951733586fbaf
bambamboole <manuel.christlieb@xentral.com>
2025-03-08 20:14:01 +0100

    First base structure for vitepress docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Siteman.io",
+  description: "Your website manager",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Docs', link: '/getting-started/introduction' }
+    ],
+
+    sidebar: [
+        {
+            text: 'Getting Started',
+            items: [
+                { text: 'Introduction', link: '/getting-started/introduction' },
+                { text: 'Installation', link: '/getting-started/installation' },
+            ]
+        },
+        {
+            text: 'Features',
+            items: [
+                { text: 'Page Types', link: '/features/page-types' },
+                { text: 'Themes', link: '/features/themes' },
+                { text: 'Blocks', link: '/features/blocks' },
+                { text: 'Layouts', link: '/features/layouts' },
+                { text: 'Menus', link: '/features/menus' },
+                { text: 'Settings', link: '/features/settings' },
+            ]
+        }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/siteman-o/cms' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Siteman.io"
+  text: "Your website manager"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Docs
+      link: /getting-started/introduction
+    - theme: alt
+      text: Installation
+      link: /getting-started/installation
+
+features:
+  - title: Simple and flexible
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Block based
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Customizable
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
