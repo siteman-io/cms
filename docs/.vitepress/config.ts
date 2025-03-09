ce46493b2baf386a0148629686fb489a462cdf89
bambamboole <manuel.christlieb@xentral.com>
2025-03-09 12:39:19 +0100

    Remove base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
     title: "Siteman.io",
     description: "Your website manager",
-    base: "/cms/",
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
