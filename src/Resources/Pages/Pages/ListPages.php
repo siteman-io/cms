d314e6ba8d8d91e5eab280204f3e649f7aefa4aa
bambamboole <manuel.christlieb@xentral.com>
2025-06-21 10:35:04 +0200

    reset list pages table

--- src/Resources/Pages/Pages/ListPages.php
+++ src/Resources/Pages/Pages/ListPages.php
@@ -2,46 +2,16 @@
 
 namespace Siteman\Cms\Resources\Pages\Pages;
 
-use Filament\Resources\Pages\Page;
-use Livewire\Attributes\On;
-use Livewire\Attributes\Url;
-use Siteman\Cms\Resources\HasPreviewModal;
+use Filament\Resources\Pages\ListRecords;
 use Siteman\Cms\Resources\Pages\Actions\CreateAction;
 use Siteman\Cms\Resources\Pages\PageResource;
 
-class ListPages extends Page
+class ListPages extends ListRecords
 {
-    use HasPreviewModal;
-
     protected static string $resource = PageResource::class;
-
-    protected string $view = 'siteman::resources.page.pages.list-pages';
-
-    #[Url]
-    public ?int $selectedPageId = null;
-
-    public function mount(): void
-    {
-        // Handle URL parameter for selectedPageId
-        if ($this->selectedPageId) {
-            // Validate that the page exists
-            $page = PageResource::getModel()::find($this->selectedPageId);
-            if (!$page) {
-                $this->selectedPageId = null;
-            }
-        }
-    }
-
-    #[On('page-selected')]
-    public function onPageSelected(int $pageId): void
-    {
-        $this->selectedPageId = $pageId;
-    }
 
     protected function getHeaderActions(): array
     {
-        //        FacadesView::share(View::PREVIEW_ACTION_SETUP_HOOK, true);
-
         return [
             CreateAction::make(),
         ];
