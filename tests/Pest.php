7ff75430052e08131e1cdf8a6bc4eabdfd1b9eb3
Manuel Christlieb <manuel.christlieb@xentral.com>
2025-11-22 19:30:48 +0100

    fix login browser test yeah !!!! add simple dashboard browser test

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,3 +3,8 @@
 use Siteman\Cms\Tests\TestCase;
 
 uses(TestCase::class)->in('Feature', 'Browser');
+
+function createUser(array $state = []): \Workbench\App\Models\User
+{
+    return \Workbench\App\Models\User::factory()->create($state);
+}
